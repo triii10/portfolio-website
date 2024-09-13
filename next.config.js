e36f2c251e9e8ef9d129a129c6acdf8535618abc
Trilok Bhattacharya <triii.10@gmail.com>
2024-09-13 18:22:33 +0100

    adding next.config.mjs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
-
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -14,9 +12,5 @@ const nextConfig = {
     serverActions: true,
   },
 };
-
-if (process.env.NODE_ENV === 'development') {
-  await setupDevPlatform();
-}
 
 module.exports = nextConfig;
